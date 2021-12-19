fb1a7f1483108b1987c982658e9837a10fcc18a5
prathviraj.gavani <prathviraj.gavani@servicenow.com>
2021-12-19 19:12:39 +0100

    Registrer user & save on postgres

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,8 +1,50 @@
-export default function (req, res) {
+import { User } from '../../../model.js'
+
+
+const randomString = (length) => {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = length; i > 0; --i) {
+        result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+}
+
+
+
+export default async function (req, res) {
     if (req.method !== 'POST') {
         res.status(405).end() //Method Not Allowed
         return;
     }
-    console.log(req.body);
-    res.end();
+
+
+    const { email, password, passwordconfirmation } = req.body;
+
+    if (password !== passwordconfirmation) {
+        res.end(
+            JSON.stringify({ status: 'error', message: 'Passwords do not match' })
+        );
+        return;
+    }
+
+    let user = await User.findOne({ where: { email } })
+
+    if (!user) {
+        user = await User.create({ email, password });
+
+        const sessionToken = randomString(255);
+        const d = new Date();
+        d.setDate(d.getDate() + 30);
+        User.update(
+            {
+                session_token: sessionToken,
+                session_expiration: d
+            },
+            { where: { email } }
+        );
+        res.end(JSON.stringify({ status: 'success', message: 'User added' }));
+    } else {
+        res.end(JSON.stringify({ status: 'error', message: 'User already exists' }));
+    }
 }
